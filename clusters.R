1f442c7d9c6a17d50a0c97345ecb1b69bfc4d12a
Jordi Bosch <jordiboschbosch@hotmail.com>
2020-10-27 18:46:26 +0100

    dividir overall score

--- clusters.R
+++ clusters.R
@@ -1,7 +1,12 @@
 library(tidyverse)  # data manipulation
 library(cluster)    # clustering algorithms
 library(factoextra)
+
+overall <- read.csv2("players.csv", sep=",")$overall
+#llegim l'overall dun altre lloc
+
 players <- read.csv2("players_def.csv",row.names = 1)
+players$overall<-overall
 
 players <- players[!duplicated(players[,c("short_name")]),]
 #aixo ens ha tret els jugadors repetits
@@ -22,11 +27,17 @@ playerscluster <- playerscluster[complete.cases(playerscluster), ]
 #Pero a l'hora de fer lultim plot nomes surtin els jugadors que ens interessen
 playerscluster <- playerscluster
 
+players_mst <- playerscluster
+
 sapply(playerscluster,class)
 #Hem d'applicar el k-means sense el valor de character
 players_numeric = playerscluster[-c(1)]
 
 players_numeric <- na.omit(players_numeric)
+#dividim per l'overall
+players_numeric <- players_numeric[,1:34]/(players_numeric[,35])
+players_numeric$overall <- NULL
+
 players_numeric <- scale(players_numeric)
 
 #players_numeric <- head(players_numeric,8000)
@@ -37,7 +48,7 @@ str(k2)
 
 quartz()
 #marc tu canvia quart per la instruccio amb windows
-fviz_cluster(k2, data = players_numeric, geom="text")
+fviz_cluster(k2, data = players_numeric)
 dev.off()
 
 #Comprovacio dels 4 grups
@@ -61,4 +72,37 @@ hist(defensegroup)
 gkgroup <- playerscluster[playerscluster['team_position']=="GK",'cluster']
 hist(gkgroup)
 
+dev.off()
+#Ara farem el MST
+library("mstknnclust")
+matriu_players = as.matrix(head(players_numeric,600))
 
+
+library("stats")
+d <- base::as.matrix(stats::dist(matriu_players, method="euclidean"))
+#cg <- generate.complete.graph(1:nrow(matriu_players),d)
+#mstree <- generate.mst(cg)
+#plot(mstree$mst.graph, main="MST")
+
+#dividirem per l'overall
+
+res <- mst.knn(d,4)
+View(res)
+
+
+
+
+players_mst <- head(players_mst,500)
+players_mst$cluster <- res$cluster
+
+midgroup_mst <- players_mst[players_mst['team_position']=="MID",'cluster']
+hist(midgroup_mst)
+
+attackgroup_mst <- players_mst[players_mst['team_position']=="ATT",'cluster']
+hist(attackgroup_mst)
+
+defensegroup_mst <- players_mst[players_mst['team_position']=="DEF",'cluster']
+hist(defensegroup_mst)
+
+gkgroup_mst <- players_mst[players_mst['team_position']=="GK",'cluster']
+hist(gkgroup_mst)
